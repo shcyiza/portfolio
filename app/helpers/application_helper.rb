4bed57d8619d3c6f1facb4943c3fc2708a31f652
shcyiza <patick.cyiza@gmail.com>
2018-06-13 15:08:18 +0200

    initial dev

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def dev_firstname
+    return "Patrick"
+  end
+
+  def dev_lastname
+    return "Cyiza"
+  end
+
+  def dev_fullname
+    return "#{dev_firstname} #{dev_lastname}"
+  end
+
 end
